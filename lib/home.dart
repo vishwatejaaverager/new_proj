6d4794664783c642511e306d02beab04745a7e6b
vishwadevawto <vishwa.dev@awto.in>
2024-01-07 14:44:28 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
